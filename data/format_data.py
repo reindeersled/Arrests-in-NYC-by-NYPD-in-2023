d55ec0945d1dd0eea3d2ae6530946df42696306a
reindeersled <reina.lin26@trinityschoolnyc.org>
2024-04-03 10:24:29 -0400

    format_data.py

--- data/format_data.py
+++ data/format_data.py
@@ -1,1 +1,17 @@
+import json
 
+
+f1 = open("trimmed_dataset.csv", "r")
+lines = f1.readlines()
+
+dictionary ={}
+
+# Create the dictionary here
+
+f1.close()
+
+#Save the json object to a file
+f2 = open("original_dataset.json", "w")
+json.dump(dictionary, f2, indent = 4)
+
+f2.close()
