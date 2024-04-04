8de399fad5d3befc6be5f347aaaf3df4ac35a58f
reindeersled <reina.lin26@trinityschoolnyc.org>
2024-04-04 09:38:59 -0400

    html

--- data/format_data.py
+++ data/format_data.py
@@ -4,9 +4,14 @@ import json
 f1 = open("data/Excel_dataset.csv", "r")
 lines = f1.readlines()
 
+# Create the dictionary here
 dictionary ={}
 
-# Create the dictionary here
+for arrest in lines:
+    arrest_details = arrest.split(',') #each arrest with details as elements in a list
+
+    dict_input = arrest_details[1:]
+    dictionary[arrest_details[0]] = dict_input
 
 f1.close()
 
