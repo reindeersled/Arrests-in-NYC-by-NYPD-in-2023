481fa1790071aed0593ab705ff0d1eae20f84da4
reindeersled <reina.lin26@trinityschoolnyc.org>
2024-04-17 16:56:30 -0400

    idk whats going on

--- server.py
+++ server.py
@@ -10,4 +10,12 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 def index():
     return render_template('index.html')
 
+@app.route('/bx.html')
+def borough(): #ok. need a seperate thing for each borough
+    return render_template('bx.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 app.run(debug=True)
